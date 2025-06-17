31418b50eef5d4a8f71639234a5627ff50b97ff0
VerTiC-dev <vovangmcom228@gmail.com>
2025-06-17 17:37:35 +0200

    First commit

---
+++ German Dictionary/German Dictionary/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace German_Dictionary
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
