de5b66591af5cda58b5e906319b63c2fa23709e6
VerTiC-dev <vovangmcom228@gmail.com>
2025-06-17 18:05:32 +0200

    Main window is rounded and can be dragged

--- German Dictionary/German Dictionary/MainWindow.axaml.cs
+++ German Dictionary/German Dictionary/MainWindow.axaml.cs
@@ -8,5 +8,12 @@ namespace German_Dictionary
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Function to handle the border pointer pressed event to allow dragging the window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Border_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e) => this.BeginMoveDrag(e);
     }
 }
