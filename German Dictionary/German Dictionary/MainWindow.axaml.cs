e3232a49573982636f47e8f06ff8534b84df3947
VerTiC-dev <vovangmcom228@gmail.com>
2025-06-19 01:11:12 +0200

    Close and minimize buttons, little changes

--- German Dictionary/German Dictionary/MainWindow.axaml.cs
+++ German Dictionary/German Dictionary/MainWindow.axaml.cs
@@ -12,8 +12,16 @@ namespace German_Dictionary
         /// <summary>
         /// Function to handle the border pointer pressed event to allow dragging the window.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Border_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e) => this.BeginMoveDrag(e);
+
+        /// <summary>
+        /// Function to handle the exit button click event to close the application.
+        /// </summary>
+        private void ButtonExit_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e) => System.Diagnostics.Process.GetCurrentProcess().Kill();
+
+        /// <summary>
+        /// Function to handle the minimize button click event to change the window state to minimized.
+        /// </summary>
+        private void ButtonMinimize_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e) => this.WindowState = WindowState.Minimized;
     }
 }
