10a0dd80ec384acc263fff65036c23fa81000ec5
VerTiC-dev <vovangmcom228@gmail.com>
2025-07-14 21:25:59 +0200

    add work with json files

---
+++ German Dictionary/German Dictionary/JSON.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace German_Dictionary
+{
+    public static class JSON
+    {
+        private const string NEW_WORDS_PATH = "words/new_words.json";
+        private const string LEARNED_WORDS_PATH = "words/learned_words.json";
+
+        public static string SerializeWords(List<Word> words)
+        {
+            return JsonSerializer.Serialize(words);
+        }
+
+        public static List<Word> DeserializeWords()
+        {
+            // just for test
+            string json = File.ReadAllText(LEARNED_WORDS_PATH);
+            return JsonSerializer.Deserialize<List<Word>>(json);
+        }
+    }
+
+    public class Word(string translation, string singular, string plural, string article, DateTime time)
+    {
+        [JsonPropertyName("translation")]
+        public string Translation { get; set; } = translation;
+
+        [JsonPropertyName("article")]
+        public string Article { get; set; } = article;
+
+        [JsonPropertyName("singular")]
+        public string Singular { get; set; } = singular;
+
+        [JsonPropertyName("plural")]
+        public string Plural { get; set; } = plural;
+
+        [JsonPropertyName("time")]
+        public DateTime Time { get; set; } = time;
+    }
+}
--- German Dictionary/German Dictionary/MainWindow.axaml.cs
+++ German Dictionary/German Dictionary/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using System;
 
 namespace German_Dictionary
 {
